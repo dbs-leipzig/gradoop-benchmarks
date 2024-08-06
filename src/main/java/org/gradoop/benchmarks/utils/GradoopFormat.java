f273256e1e4321e780ecb9ac6ec2040fdd20dfc7
ChrizZz110 <110@posteo.de>
2024-08-06 14:41:18 +0200

    [#34] Updated enum javadoc

--- src/main/java/org/gradoop/benchmarks/utils/GradoopFormat.java
+++ src/main/java/org/gradoop/benchmarks/utils/GradoopFormat.java
@@ -15,18 +15,35 @@
  */
 package org.gradoop.benchmarks.utils;
 
+/**
+ * An enum to represent the supported gradoop output formats.
+ */
 public enum GradoopFormat {
   CSV("csv"),
   INDEXED_CSV("indexed"),
   PARQUET("parquet"),
   PARQUET_PROTOBUF("protobuf");
 
+  /**
+   * A String representation of the enum, used for program arguments.
+   */
   public final String name;
 
+  /**
+   * Creates an instance of this enum.
+   *
+   * @param name the String representation
+   */
   private GradoopFormat(String name) {
     this.name = name;
   }
 
+  /**
+   * Returns the enum instance by the given String representation or {@code null} otherwise.
+   *
+   * @param name the String representation of the enum (csv, indexed, parquet or protobuf)
+   * @return the respective enum
+   */
   public static GradoopFormat getByName(String name) {
     for (GradoopFormat format : values()) {
       if (format.name.equals(name)) {
